87d6ce21acb1a594e2a96bbd2bda7499d20a294f
Luis GL <lmgl887@gmail.com>
2023-09-08 15:58:59 -0600

    add: auction database context class

--- src/AuctionService/Entities/Item.cs
+++ src/AuctionService/Entities/Item.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AuctionService.Entities;
 
+[Table("Items")]
 public class Item
 {
   public Guid Id { get; set; }
@@ -9,7 +12,7 @@ public class Item
   public string Color { get; set; }
   public int Mileage { get; set; }
   public string ImageUrl { get; set; }
-  
+
   //nav properties
   public Auction Auction { get; set; }
   public Guid AuctionId { get; set; }
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,7 +1,17 @@
+using AuctionService.Data;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Connections;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+var auctionsSettings = builder.Configuration["Auctions:ConnectionSettings"];
+
+builder.Services.AddDbContext<AuctionDbContext>(options => {
+  options.UseNpgsql(auctionsSettings);
+});
 
 
 var app = builder.Build();

268a7284d2051fb46c51bee7b6896fd7bdbdf7e2
Luis GL <lmgl887@gmail.com>
2023-09-25 16:20:24 -0600

    add: auctions controller

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,48 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+
+  private readonly AuctionDbContext _context;
+  private readonly IMapper _mapper;
+
+  public AuctionsController(AuctionDbContext context, IMapper mapper)
+  {
+    _context = context;
+    _mapper = mapper;
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+  {
+    var auctions = await _context.Auctions
+      .Include(auction => auction.Item)
+      .OrderBy(auction => auction.Item.Make)
+    .ToListAsync();
+
+    return _mapper.Map<List<AuctionDto>>(auctions);
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+  {
+    var auction = await _context.Auctions
+      .Include(auction => auction.Item)
+      .FirstOrDefaultAsync(auction => auction.Id == id);
+
+    if (auction is null) return NotFound();
+
+    
+    return _mapper.Map<AuctionDto>(auction);
+
+  }
+
+}

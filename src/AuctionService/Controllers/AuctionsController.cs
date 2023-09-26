50d9551b2bbdb70092c379f6d612f924d00401fb
Luis GL <lmgl887@gmail.com>
2023-09-26 12:51:26 -0600

    add: update auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -67,4 +67,29 @@ public class AuctionsController : ControllerBase
 
   }
 
+  [HttpPut("{id}")]
+  public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+  {
+
+    var auction = await _context.Auctions.Include(auction => auction.Item)
+    .FirstOrDefaultAsync(auction => auction.Id == id);
+
+
+    if (auction is null) return NotFound();
+
+    //TODO: check seller == username
+
+    auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+    auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+    auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+    auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+    auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+    var result = await _context.SaveChangesAsync() > 0;
+
+    if (result) return Ok();
+
+    return BadRequest("There was an error at update");
+  }
+
 }

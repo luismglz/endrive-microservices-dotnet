080feade97ebec689c1ef46b9fd38513bbaa3695
Luis GL <lmgl887@gmail.com>
2023-09-27 15:12:11 -0600

    add: item model and db connection

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,10 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var searchServiceSettings = builder.Configuration["Search:ConnectionSettings"];
 
 // Add services to the container.
 
@@ -9,5 +15,19 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync(
+  "searchDB",
+  MongoClientSettings.FromConnectionString(searchServiceSettings)
+  );
+
+
+//add index to those Item class props for mongo
+
+await DB.Index<Item>()
+  .Key(item => item.Make, KeyType.Text)
+  .Key(item => item.Model, KeyType.Text)
+  .Key(item => item.Color, KeyType.Text)
+.CreateAsync();
 
 app.Run();

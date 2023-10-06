74501b1ca9634dc9246d23fa8d5a0dac6db1ebfd
Luis GL <lmgl887@gmail.com>
2023-10-05 23:36:00 -0600

    add: user data object creation in signup

--- src/IdentityService/Pages/Account/Signup/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Signup/Index.cshtml.cs
@@ -1,4 +1,8 @@
+using System.Security.Claims;
+using IdentityModel;
+using IdentityService.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,8 +12,61 @@ namespace IdentityService.Pages.Signup
     [AllowAnonymous]
     public class Index : PageModel
     {
-        public void OnGet()
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public Index(UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public SignupViewModel Input { get; set; }
+
+
+        [BindProperty]
+        public bool RegisterSuccess { get; set; }
+
+
+
+        public IActionResult OnGet(string returnUrl)
+        {
+            Input = new SignupViewModel
+            {
+                ReturnUrl = returnUrl,
+            };
+
+            return Page();
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Input.Button != "signup") return Redirect("~/");
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Username,
+                    Email = Input.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddClaimsAsync(user, new Claim[]{
+                        new Claim(JwtClaimTypes.GivenName, Input.FirstName),
+                        new Claim(JwtClaimTypes.FamilyName, Input.LastName)
+                    });
+
+                    RegisterSuccess = true;
+                }
+            }
+
+            return Page();
         }
     }
 }

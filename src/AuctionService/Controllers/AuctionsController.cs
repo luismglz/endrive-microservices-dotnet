6f2d3233202b4d9617c7b609d30f22825c0b7863
Luis GL <lmgl887@gmail.com>
2023-10-06 23:19:53 -0600

    add: auth endpoints configuration on the resource service

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Contracts;
 using MassTransit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -56,13 +57,15 @@ public class AuctionsController : ControllerBase
 
   }
 
-
+  [Authorize]
   [HttpPost]
   public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
   {
 
     var auction = _mapper.Map<Auction>(auctionDto);
-    auction.Seller = "Test";
+
+    auction.Seller = User.Identity.Name;
+
     _context.Auctions.Add(auction);
 
     var newAuction = _mapper.Map<AuctionDto>(auction);
@@ -83,6 +86,7 @@ public class AuctionsController : ControllerBase
   }
 
 
+  [Authorize]
   [HttpPut("{id}")]
   public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
   {
@@ -93,7 +97,7 @@ public class AuctionsController : ControllerBase
 
     if (auction is null) return NotFound();
 
-    //TODO: check seller == username
+    if (auction.Seller != User.Identity.Name) return Forbid();
 
     auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
     auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
@@ -111,6 +115,7 @@ public class AuctionsController : ControllerBase
   }
 
 
+  [Authorize]
   [HttpDelete("{id}")]
   public async Task<ActionResult> DeleteAuction(Guid id)
   {
@@ -118,7 +123,7 @@ public class AuctionsController : ControllerBase
 
     if (auction is null) return NotFound();
 
-    //TODO: check seller == username
+    if (auction.Seller != User.Identity.Name) return Forbid();
 
     _context.Auctions.Remove(auction);
 

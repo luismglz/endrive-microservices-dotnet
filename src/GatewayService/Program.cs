ae8f1e317037cc0bf00e0fefd6db2b438ac5d50d
Luis GL <lmgl887@gmail.com>
2023-10-07 22:40:40 -0600

    add: authentication rules to gateway config

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var IdentityServiceUrl = builder.Configuration["Auctions:IdentityServiceUrl"];
+
 
 builder.Services.AddReverseProxy()
   .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+  options.Authority = IdentityServiceUrl;
+  options.RequireHttpsMetadata = false;
+  options.TokenValidationParameters.ValidateAudience = false;
+  options.TokenValidationParameters.NameClaimType = "username";
+
+});
 
 
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();

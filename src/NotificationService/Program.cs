57311fad1cb9b81c23393f3ef46769bc3f4e1fc0
Luis GL <lmgl887@gmail.com>
2023-10-17 18:09:47 -0600

    add: bid placed consumer

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMassTransit(config =>
 {
+  config.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
   config.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
   config.UsingRabbitMq((context, cfg) =>
   {
